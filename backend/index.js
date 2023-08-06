88ec5155c48bff4b20e4a19610013fa393226d5d
aadityabansalgithubcoding <88270439+aadityabansalgithubcoding@users.noreply.github.com>
2023-08-07 00:50:29 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const app = express()
-//const path=require('path');
+const {config} = require('dotenv')
+const path=require('path');
 const port = 5000
 const mongoDB= require("./db.js");
 
 
 // const __dirname=path.resolve();
-app.use(express.static(path.join(__dirname,'/frontend/build')))
+app.use(express.static(path.join(__dirname,'./../frontend/build')))
 app.get('*',(req,res)=>
-res.sendFile(path.join(__dirname,'/frontend/build/index.html')))
+res.sendFile(path.join(__dirname,'./../frontend/build/index.html')))
 
 
 
