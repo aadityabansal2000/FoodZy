e6890b6946800e770a616902e2d058caa4ffec80
Aaditya Bansal <108510402+aadityabansal2000@users.noreply.github.com>
2024-09-01 12:11:18 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,5 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-// const mongoURI="mongodb+srv://Aaditya_Bansal:mern123@cluster0.p1iot8a.mongodb.net/gofoodmern?retryWrites=true&w=majority";
-// const mongoURI="mongodb://Aaditya_Bansal:mern123@ac-lvzzdfc-shard-00-00.p1iot8a.mongodb.net:27017,ac-lvzzdfc-shard-00-01.p1iot8a.mongodb.net:27017,ac-lvzzdfc-shard-00-02.p1iot8a.mongodb.net:27017/gofoodmern?ssl=true&replicaSet=atlas-wruy7u-shard-0&authSource=admin&retryWrites=true&w=majority";
 const connectToMongo =  () => {
     mongoose.connect(process.env.mongoURI)
     .then( ()=>
